d91f47aabfd68aa6493e4b581c0fddac24a0f406
Anuja Sharma <asharma1198@gmail.com>
2020-09-17 17:54:25 +0100

    Fixed my commenting!

--- cachematrix.R
+++ cachematrix.R
@@ -6,18 +6,18 @@
 makeCacheMatrix <- function(x = matrix()) {
         inv <- NULL 
         
-        # Line 14 defines a function to set the matrix, X, to a new matrix, Y, and 
+        # The set line defines a function to set the matrix, X, to a new matrix, Y, and 
         # resets the inverse, inv, to NULL.
         set <- function(Y) {
                 X <<- Y
                 inv <<- NULL
         }
         
-        get <- function() X # returns the matrix, x 
+        get <- function() X # returns the matrix, X 
         
-        setinverse <- function(inverse) inv <<- inverse  # sets the mean, m, to mean
+        setinverse <- function(inverse) inv <<- inverse  # sets the inverse of the matrix, inv, to inverse
         
-        getinverse <- function() inv # returns the mean, m
+        getinverse <- function() inv # returns the inverse, inv
         
         list(set = set,       
              get = get,         
@@ -29,13 +29,16 @@ makeCacheMatrix <- function(x = matrix()) {
 ## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache. For this assignment, we're assuming that the matrix supplied is always invertible.
 
 cacheSolve <- function(X, ...) {
-        ## Return a matrix that is the inverse of 'X'
+        ## This function returns a matrix that is the inverse of 'X'
         
+        # It first checks the cache memory 
         inv <- X$getinverse()
         if(!is.null(inv)) {
                 message("getting cached data")
                 return(inv)
         }
+        
+        # If the inverse of this matrix hasn't already been solved, then it will do so now, and then save it back into the object X.
         data <- X$get()
         inv <- solve(data, ...)
         X$setinverse(inv)
